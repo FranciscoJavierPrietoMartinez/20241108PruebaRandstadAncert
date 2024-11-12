e71e28fa919c5a54da344ebdc0332645cb2695ad
FranciscoJavierPrietoMartinez <fjprieto.trabajo@gmail.com>
2024-11-12 04:27:57 +0100

    Add files via upload
    Carga inicial de la carpeta del proyecto Ancert

---
+++ AncertCrono/ViewModel/CronoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AncertCrono
+{
+    class CronoViewModel
+    {
+        Crono _crono = null;
+
+        public CronoViewModel()
+        {
+            _crono = new Crono();
+        }
+
+        public Crono MyCrono
+        {
+            get { return _crono; }
+        }
+    }
+}
